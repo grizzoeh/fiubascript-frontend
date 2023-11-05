c420c8f266652f4ed548d1e0a718c2ef842c6bf7
Lucas Rial Brandariz <lrial@fi.uba.ar>
2023-11-05 19:49:50 -0300

    Add background container

---
+++ fiubascript/src/components/BackgroundContainer.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react'
+import { ReactNode } from 'react';
+import background from '../assets/background.png';
+import { COLORS } from '../constants/constants';
+
+type BackgroundContainerProps = {
+  children: ReactElement;
+};
+
+export const BackgroundContainer = ({ children } : BackgroundContainerProps) => {
+  return (
+    <div style={{ 
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor: COLORS.primaryDark,
+      backgroundPosition: 'center',
+      minHeight: '100vh',
+      padding: '1rem',
+    }}>
+      {children}
+    </div>
+  );
+};
+

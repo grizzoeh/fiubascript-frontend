d0d3d762cbde5cfb83516c1ea265462cecce2623
grizzoeh <gonzarizzoeh@gmail.com>
2023-11-06 17:51:57 -0300

    add onrender regitser and login endpoints

--- fiubascript/src/login/Login.tsx
+++ fiubascript/src/login/Login.tsx
@@ -10,7 +10,7 @@ export const Login = () => {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try{
-            const response = await fetch("http://localhost:8080/auth/login", {
+            const response = await fetch("https://fiubascript-backend.onrender.com/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"

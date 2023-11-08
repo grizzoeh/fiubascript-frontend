113d31418bb2038cacf22b30b98a45759b20135a
CAROLINA PICO <caropico00@gmail.com>
2023-11-08 20:13:45 -0300

    Frontend home screen

--- fiubascript/src/home/Home.tsx
+++ fiubascript/src/home/Home.tsx
@@ -1,7 +1,23 @@
+import React from 'react'
+import { BackgroundContainer } from '../components/BackgroundContainer'
+import { HomeButton } from '../components/HomeButton'
+import triviaTitle from '../assets/TrivIA.png';
+import Jugar from '../assets/Jugar.png';
+import Tienda from '../assets/Tienda.png';
+import './Home.css';
+
 export const Home = () => {
     return(
-        <div className="container">
-            logged in home
+        <BackgroundContainer>
+        <div className="centered-image">
+        <img src={triviaTitle} alt="TrivIA Title" />
         </div>
+        <div className="centered-content">
+        <div className="button-row">
+        <HomeButton title="Jugar" imageSrc={Jugar}></HomeButton>
+        <HomeButton title="Tienda" imageSrc={Tienda}></HomeButton>
+        </div>
+        </div>
+        </BackgroundContainer>
     )
 }
